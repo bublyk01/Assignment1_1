db62f642929b3f3857b61b626a4bbf614a36a412
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-05-25 16:25:05 +0300

    If cycle with commands + text input function
    All of the commands were added to the if cycle, along with the if cycle itself; text input was created as a dedicated function, which is called when command == 1

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
+#include <stdio.h>
+#include <print>
 
-int main()
+int text_input();
+int main() {
+    int command;
+
+    printf("Start the new line\n");
+    printf("Choose the command: ");
+    scanf_s("%d", &command);
+
+    if (command == 1) {
+        printf("Enter text to append: \n");
+        text_input();
+    }
+    else if (command == 2) {
+        printf("New line is started");
+    }
+    else if (command == 3) {
+        printf("Enter the file name for saving:\n Text has been saved succesfully");
+    }
+    else if (command == 4) {
+        printf("Enter the file name for loading:\n Text has been loaded succesfully");
+    }
+    else if (command == 5) {
+
+    }
+    else if (command == 6) {
+
+    }
+    else if (command == 7) {
+
+    }
+    else {
+        printf("Invalid command\n");
+    }
+
+    return 0;
+}
+int text_input()
 {
     std::string text;
-
     std::cout << "Enter the text: ";
     std::cin >> text;
-
     std::cout << text;
+
     return 0;
 }
